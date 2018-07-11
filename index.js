5831e4708add39c5bbcdd773c26d099b87dd17de
Rayan Dutta <rondutta23@gmail.com>
2018-07-11 09:18:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,7 +40,11 @@ function viewCart() {
 }
 
 function total() {
-  // write your code here
+  var total = 0;
+  for (let i=0; i<cart.length; i++) {
+    total = total + cart[i].itemPrice
+  }
+  return total
 }
 
 function removeFromCart(item) {
